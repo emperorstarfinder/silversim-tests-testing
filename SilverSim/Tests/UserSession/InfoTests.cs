7a6651506f27c38f9e4677cc0c612ccda34af59c
ft <ft@noemail>
2018-10-28 15:25:24 +0100

    add UserSessionInfo retrieve tests

---
+++ SilverSim/Tests/UserSession/InfoTests.cs
@@ -0,0 +1,316 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3 with
+// the following clarification and special exception.
+
+// Linking this library statically or dynamically with other modules is
+// making a combined work based on this library. Thus, the terms and
+// conditions of the GNU Affero General Public License cover the whole
+// combination.
+
+// As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent
+// modules, and to copy and distribute the resulting executable under
+// terms of your choice, provided that you also meet, for each linked
+// independent module, the terms and conditions of the license of that
+// module. An independent module is a module which is not derived from
+// or based on this library. If you modify this library, you may extend
+// this exception to your version of the library, but you are not
+// obligated to do so. If you do not wish to do so, delete this
+// exception statement from your version.
+
+using log4net;
+using Nini.Config;
+using SilverSim.Main.Common;
+using SilverSim.ServiceInterfaces.UserSession;
+using SilverSim.Tests.Extensions;
+using SilverSim.Types;
+using SilverSim.Types.UserSession;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+
+namespace SilverSim.Tests.UserSession
+{
+    public sealed class InfoTests : ITest
+    {
+        private static readonly ILog m_Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private UserSessionServiceInterface m_UserSessionService;
+
+        public void Startup(ConfigurationLoader loader)
+        {
+            IConfig config = loader.Config.Configs[GetType().FullName];
+            m_UserSessionService = loader.GetService<UserSessionServiceInterface>(config.GetString("UserSessionService"));
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public void Cleanup()
+        {
+
+        }
+
+        public bool Run()
+        {
+            UUID sessionID = new UUID("11223344-1122-1122-1122-112233445566");
+            UUID secureSessionID = new UUID("11223344-1122-1122-1133-112233445566");
+            UGUIWithName uguiname = new UGUIWithName("11223344-1111-1122-1122-112233445566", "http://example.com/;Example Com");
+            UGUI ugui = new UGUI(uguiname);
+            string clientipaddress = "127.0.0.1";
+
+            m_Log.Info("Creating session");
+            UserSessionInfo createdSession = m_UserSessionService.CreateSession(uguiname, clientipaddress, sessionID, secureSessionID);
+
+
+            m_Log.Info("Creating value 1");
+            m_UserSessionService.SetExpiringValue(sessionID, "testassoc1", "testvarname1", "testvalue1", TimeSpan.FromSeconds(60));
+
+            m_Log.Info("Creating value 2");
+            m_UserSessionService[sessionID, "testassoc2", "testvarname2"] = "testvalue2";
+
+            UserSessionInfo info;
+            UserSessionInfo.Entry e;
+
+            m_Log.Info("Fetching sessioninfo 1");
+            info = m_UserSessionService[sessionID];
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if(e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+
+            m_Log.Info("Fetching sessioninfo 2");
+            if (!m_UserSessionService.TryGetValue(sessionID, out info))
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+
+            m_Log.Info("Fetching sessioninfo 3");
+            if (!m_UserSessionService.TryGetSecureValue(secureSessionID, out info))
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+
+            m_Log.Info("Fetching sessioninfo 4");
+            List<UserSessionInfo> list = m_UserSessionService[ugui];
+            if (list.Count != 1)
+            {
+                return false;
+            }
+            info = list[0];
+
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+
+            m_Log.Info("Creating value 3");
+            m_UserSessionService.SetExpiringValue(sessionID, "testassoc3", "testvarname3", "testvalue3", TimeSpan.FromSeconds(1));
+            m_Log.Info("Wait ~2s for expiry");
+            Thread.Sleep(2000);
+
+            m_Log.Info("Fetching sessioninfo 1");
+            info = m_UserSessionService[sessionID];
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for expiry of value 3");
+            if(info.ContainsKey("testassoc3", "testvarname3"))
+            {
+                return false;
+            }
+
+            m_Log.Info("Fetching sessioninfo 2");
+            if (!m_UserSessionService.TryGetValue(sessionID, out info))
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for expiry of value 3");
+            if (info.ContainsKey("testassoc3", "testvarname3"))
+            {
+                return false;
+            }
+
+            m_Log.Info("Fetching sessioninfo 3");
+            if (!m_UserSessionService.TryGetSecureValue(secureSessionID, out info))
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for expiry of value 3");
+            if (info.ContainsKey("testassoc3", "testvarname3"))
+            {
+                return false;
+            }
+
+            m_Log.Info("Fetching sessioninfo 4");
+            list = m_UserSessionService[ugui];
+            if (list.Count != 1)
+            {
+                return false;
+            }
+            info = list[0];
+
+            m_Log.Info("Testing for value 1");
+            if (!info.TryGetValue("testassoc1", "testvarname1", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 1");
+            if (e.Value != "testvalue1" || e.ExpiryDate == null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for value 2");
+            if (!info.TryGetValue("testassoc2", "testvarname2", out e))
+            {
+                return false;
+            }
+            m_Log.Info("Testing equality of value 2");
+            if (e.Value != "testvalue2" || e.ExpiryDate != null)
+            {
+                return false;
+            }
+            m_Log.Info("Testing for expiry of value 3");
+            if (info.ContainsKey("testassoc3", "testvarname3"))
+            {
+                return false;
+            }
+
+            m_UserSessionService.Remove(sessionID);
+
+            return true;
+        }
+    }
+}
